eb730d5e7d927b47ed9bf67fff0d1e1221d38b81
Brutski93 <106704692+Brutski93@users.noreply.github.com>
2025-09-07 13:52:27 +0300

    init: before linrer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  const arr = [];
+  if (matrix == undefined) return [];
+  for (let i = 0; i < matrix.length; i++){
+    if (i % 2 === 0) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        arr.push(matrix[i][j]);
+      }
+    } else {
+      for (let j =  matrix[i].length - 1; j >= 0; j--) {
+        arr.push(matrix[i][j]);
+      }
+     }
+  }
+  return arr;
 };
