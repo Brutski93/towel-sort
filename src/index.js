be06af25243c2b871a8a04bdef0122ece14f0f8f
Brutski93 <abrutski@gmail.com>
2025-09-07 21:38:02 +0300

    fix: fix fuking lint

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 module.exports = function towelSort(matrix) {
   const arr = [];
-  if (matrix == undefined) return [];
-  for (let i = 0; i < matrix.length; i++){
+  if (matrix === undefined) return [];
+  for (let i = 0; i < matrix.length; i += 1) {
     if (i % 2 === 0) {
-      for (let j = 0; j < matrix[i].length; j++) {
+      for (let j = 0; j < matrix[i].length; j += 1) {
         arr.push(matrix[i][j]);
       }
     } else {
-      for (let j =  matrix[i].length - 1; j >= 0; j--) {
+      for (let j = matrix[i].length - 1; j >= 0; j -= 1) {
         arr.push(matrix[i][j]);
       }
-     }
+    }
   }
   return arr;
 };
